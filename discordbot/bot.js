12ad864bcb17ef447c1eb3faaf9cd1e584434f5d
ARK-IIS-01 <zachtownes@gmail.com>
2021-03-12 21:48:51 -0700

    Changed logging

--- discordbot/bot.js
+++ discordbot/bot.js
@@ -18,9 +18,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    console.log(message)
+    //console.log(message)
     if (message.author.bot){
         if(message.author.username === 'Grafana'){
+            console.log(message.title)
             if (message.title.includes('[Alerting]')){
                 message.channel.send(`${message.author} I can't believe it's broken! I'll get @Checkovsky to try to fix it.`)
             }
