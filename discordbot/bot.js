774d798405b17fa885cfc7e1cc3b64c3b0b9a602
ARK-IIS-01 <zachtownes@gmail.com>
2021-03-12 20:22:59 -0700

    Changed content to title

--- discordbot/bot.js
+++ discordbot/bot.js
@@ -21,7 +21,7 @@ client.on('message', message => {
     console.log(message)
     if (message.author.bot){
         if(message.author.username === 'Grafana'){
-            if (message.content.includes('[Alerting]')){
+            if (message.title.includes('[Alerting]')){
                 message.channel.send(`${message.author} I can't believe it's broken! I'll get @Checkovsky to try to fix it.`)
             }
         }
